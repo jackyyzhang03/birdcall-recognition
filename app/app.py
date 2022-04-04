243ca9b7e0110583813c362a9a73de824da7085a
Jacky Zhang <jackyyzhang@gmail.com>
2022-04-03 21:05:18 -0400

    Add .dockerignore

--- app/app.py
+++ app/app.py
@@ -23,4 +23,5 @@ def predict():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='localhost', port='8080')
+    app.run(host='localhost', port=port)
+    
